05ff8df0671cb859855b92b3c3dcb4735246e588
mfrancza <mfrancza@gmail.com>
2023-03-11 14:14:00 -0600

    Remove Unnecessary Platforms
    Before:
    -Browers test and native platform builds were included unnessarily
    
    After:
    -Unnecessary build operations are not included

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,23 +24,7 @@ kotlin {
     }
     js(IR) {
         nodejs()
-        browser {
-            testTask {
-                useKarma {
-                    useFirefox()
-                }
-            }
-        }
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-
     
     sourceSets {
         val commonMain by getting {
@@ -57,8 +41,6 @@ kotlin {
         val jvmTest by getting
         val jsMain by getting
         val jsTest by getting
-        val nativeMain by getting
-        val nativeTest by getting
     }
 }
 
